aecd205802170ccd49cc3317cb88bed983aa23bd
刘斌 <xdliubin@gmail.com>
2018-01-24 14:42:19 +0800

    update

--- test/Program.cs
+++ test/Program.cs
@@ -9,7 +9,8 @@ namespace test
             string test = "hello world";
             string test2 = test + "!!!";
             string test3 = test2 + "...";
-            Console.WriteLine(test3);
+            string test4 = test3 + ",,,";
+            Console.WriteLine(test4);
         }
     }
 }
