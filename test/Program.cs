31a22cec5147af99e69bdc71befd952d2214eee9
刘斌 <xdliubin@gmail.com>
2018-01-25 08:47:52 +0800

    update

--- test/Program.cs
+++ test/Program.cs
@@ -6,12 +6,9 @@ namespace test
     {
         static void Main(string[] args)
         {
-            string test = "hello world";
-            string test2 = test + "!!!";
-            string test3 = test2 + "...";
-            string test4 = test3 + ",,,";
-            string test5 = test4 + "eee";
-            Console.WriteLine(test5);
+            string test = "hello world！！！";
+            
+            Console.WriteLine(test);
         }
     }
 }
